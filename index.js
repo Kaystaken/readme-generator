e992d69b6ec73027601814996cc566878adaea81
Kay <hiebertkay@gmail.com>
2023-12-06 20:50:53 -0500

    installed package.json and inquirer@8.2.4, created generateMarkdown.js and index.js

---
+++ index.js
@@ -0,0 +1,62 @@
+// TODO: Include packages needed for this application
+const inquier = require('inquirer');
+const fs = require('fs');
+
+const generateMarkdown = ({title, Description, TableOfContents, Installation, Usages, Contribution, Tests, Questions}) =>
+`
+`;
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: 'prompt',
+    name: 'title',
+    message: 'What is the app called?',
+  },
+{
+  type: 'prompt',
+  name: 'description',
+  message: 'What is the app for?',
+},
+{
+  type: 'prompt',
+  name: 'tableOfContents',
+  message: 'Please Write Table of Contents',
+},
+{
+  type: 'prompt',
+  name: 'installation',
+  message: 'Please Specify Installation Instructions',
+},
+{
+  type: 'prompt',
+  name: 'usages',
+  message: 'Enter Usage Information',
+},
+{
+  type: 'prompt',
+  name: 'contributions',
+  message: 'Contribution Guidelines',
+},
+{
+  type: 'prompt',
+  name: 'tests',
+  message: '',
+},
+{
+  type: 'prompt',
+  name: 'githubUsername',
+  message: 'Please Enter GitHub Username',
+},
+];
+inquirer
+  .prompt(questions)
+  .then((answers) => {
+    const readmeContent = generateMarkdown(answers);
+    fs.writeToFile('README.md', readmeContent);
+  })
+  .catch((error) => {
+    console.log(error)
+  });
+// TODO: Create a function to write README file
+
+//paste README here and ${description} ${tableOfContents} {installation} ${usages} ${contribution} ${tests} ${questions}
